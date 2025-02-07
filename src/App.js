8d91ea0de7edd5f9ed16632ce4009be0375f0e24
suhaibks <suhaibcoorg5@gmail.com>
2025-02-07 20:22:51 +0530

    added notfound

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ import Home from './pages/Home';
 import BlogPost from './pages/BlogPost';
 import About from './pages/About';
 import Contact from './pages/Contact';
+import NotFound from './pages/NotFound';
 import './App.css';
 
 function App() {
@@ -21,6 +22,7 @@ function App() {
             <Route path="/blog/:postId" element={<BlogPost />} />
             <Route path="/about" element={<About />} />
             <Route path="/contact" element={<Contact />} />
+            <Route path="*" element={<NotFound />} />
             <Route path="/blog/:postId" element={<BlogPost />}/>
           </Routes>
         </main>
