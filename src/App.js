b720b85d2cc699dce6e1df868124bc1c4ac2c916
suhaibks <suhaibcoorg5@gmail.com>
2025-02-07 19:57:14 +0530

    removed notfound page

--- src/App.js
+++ src/App.js
@@ -7,7 +7,6 @@ import Home from './pages/Home';
 import BlogPost from './pages/BlogPost';
 import About from './pages/About';
 import Contact from './pages/Contact';
-import NotFound from './pages/NotFound';
 import './App.css';
 
 function App() {
@@ -22,7 +21,6 @@ function App() {
             <Route path="/blog/:postId" element={<BlogPost />} />
             <Route path="/about" element={<About />} />
             <Route path="/contact" element={<Contact />} />
-            <Route path="*" element={<NotFound />} />
             <Route path="/blog/:postId" element={<BlogPost />}/>
           </Routes>
         </main>
